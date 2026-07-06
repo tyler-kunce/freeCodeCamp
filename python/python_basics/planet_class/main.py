c5c50d33c04c9c0f597de945305a6a3e598a2bba
Tyler Kunce <142066172+tyler-kunce@users.noreply.github.com>
2026-07-06 15:26:47 -0500

    String correction
    Corrected the string raised in ValueError to pass fCC tests.

--- python/python_basics/planet_class/main.py
+++ python/python_basics/planet_class/main.py
@@ -3,19 +3,19 @@ class Planet:
         if not isinstance(name, str):
             raise TypeError('name, planet type, and star must be strings')
         if name == '':
-            raise ValueError('name, planet type, and star must be non-empty strings')
+            raise ValueError('name, planet_type, and star must be non-empty strings')
         self.name = name
 
         if not isinstance(planet_type, str):
             raise TypeError('name, planet type, and star must be strings')
         if planet_type == '':
-            raise ValueError('name, planet type, and star must be non-empty strings')        
+            raise ValueError('name, planet_type, and star must be non-empty strings')        
         self.planet_type = planet_type
 
         if not isinstance(star, str):
             raise TypeError('name, planet type, and star must be strings')
         if star == '':
-            raise ValueError('name, planet type, and star must be non-empty strings')        
+            raise ValueError('name, planet_type, and star must be non-empty strings')        
         self.star = star
 
     def orbit(self):
